72948e824a1180185aca0cedf8e1c81c3e2e2c32
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2025-04-16 11:27:15 +0530

    storing data in database basics

--- temp_cutting/newproj/newapp/views.py
+++ temp_cutting/newproj/newapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
 
 # Create your views here.
 
@@ -17,9 +18,9 @@ def reg(request):
     return render(request, "reg.html")
 
 def registerdata(request):
-    print(request.method)
-    print(request.POST)
-    print(request.GET)
+    # print(request.method)
+    # print(request.POST)
+    # print(request.GET)
     # print(request.FILES)
     # print(request.COOKIES)
     # print(request.META)
@@ -31,11 +32,26 @@ def registerdata(request):
     dob= request.POST.get('dob')
     subscribe= request.POST.getlist('subscribe')
     gender= request.POST.get('gender')
+    image= request.POST.get('image')
+    file= request.POST.get('file')
     password= request.POST.get('password')
     cpassword= request.POST.get('cpassword')
-    print(username, email, detail, phone, dob, subscribe, gender, password, cpassword)
+    user = Student.objects.filter(stu_email=email)
+    if user:
+        msg="User Already Exist"
+        return render (request, 'reg.html', {'msg':msg})
+    else :
+        Student.objects.create(stu_email=email)
+        msg="Registration Successful!"
+        return render (request, 'login.html', {'msg':msg})
+    # print(username, email, detail, phone, dob, subscribe, gender, password, cpassword)
+
+    Student.objects.create(stu_name=username, stu_email=email, stu_dis=detail, stu_contact=phone, stu_dob=dob, stu_quali=subscribe, stu_gender=gender, stu_image=image, stu_file=file,stu_password=password)
+
+
 
 
 def login(request):
+
     return render(request, "login.html")
 
